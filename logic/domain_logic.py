19acdd8bbffd7f6458edb68de47bbdf853b778ad
charleswaithaka24 <charleswaithaka24@gmail.com>
2026-01-25 18:21:54 +0300

    added courses

--- logic/domain_logic.py
+++ logic/domain_logic.py
@@ -3,10 +3,19 @@ class Student:
     self.name=name
     self.courses=[]
 
+    def name():
+      self.name
+
 class Course:
   def __init__(self,course_code,course_name):
     self.course_code=course_code
     self.course_name=course_name
 
-def enroll_student(Student,Course):
-   pass
+course_1=Course(1100,"python")
+course_2=Course(1200,"c++")
+course_3=Course(1300,"javascript")
+course_4=Course(1400,"assembly")
+
+
+def enroll_student(name,course):
+   print(f"{name} is pursuing {course}")
