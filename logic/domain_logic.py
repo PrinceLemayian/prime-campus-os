e431200fb497259114da647a102351f9eae0629f
charleswaithaka24 <charleswaithaka24@gmail.com>
2026-01-25 17:54:28 +0300

    add logic classes

---
+++ logic/domain_logic.py
@@ -0,0 +1,12 @@
+class Student:
+  def __init__(self,name):
+    self.name=name
+    self.courses=[]
+
+class Course:
+  def __init__(self,course_code,course_name):
+    self.course_code=course_code
+    self.course_name=course_name
+
+def enroll_student(Student,Course):
+   pass
