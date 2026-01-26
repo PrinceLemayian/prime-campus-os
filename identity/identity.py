f82fa7b76c9b1e0df1a70d3df8ee0bf6c9f3c813
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-26 15:26:32 +0300

    set base classes

--- identity/identity.py
+++ identity/identity.py
@@ -3,7 +3,15 @@ class User:
         self.username = username
         self.role = role # student or admin
         
-    def login():
-        # login functionality
-        pass
+    def login(users):
+        username = input("Enter your username: ")
+        
+        for user in users:
+            if user["username"] == username:
+                print(f"Welcome, {username}!, logged in as {user['role']}")
+                return (User(username, user["role"]))
+            else:
+                print("User not found!")
+                return None
+            
     
