e8908d1ed4f9daa646bba94ffd70431003c5575b
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-26 22:14:43 +0300

    modified login method

--- identity/identity.py
+++ identity/identity.py
@@ -2,16 +2,16 @@ class User:
     def __init__(self, username, role):
         self.username = username
         self.role = role # student or admin
-        
-    def login(users):
-        username = input("Enter your username: ")
-        
-        for user in users:
-            if user["username"] == username:
-                print(f"Welcome, {username}!, logged in as {user['role']}")
-                return (User(username, user["role"]))
-            else:
-                print("User not found!")
-                return None
+
+def login(users):
+    username = input("Enter your username: ")
+    
+    for user in users:
+        if user["username"] == username:
+            print(f"Welcome, {username}!, logged in as {user['role']}")
+            return User(username, user["role"])
+        else:
+            print("User not found!")
+            return None
             
     
