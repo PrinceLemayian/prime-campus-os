61bcc9183d67e370c387945a507be03cf0ae47f3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-27 18:04:50 +0300

    added functions to enroll students in a course, and view courses, moved courses to courses.py

--- logic/domain_logic.py
+++ logic/domain_logic.py
@@ -1,17 +1,28 @@
 class Student:
   def __init__(self,name):
     self.name=name
+    self.role="Student"
     self.courses=[]
 
-  def enroll_student(self):
-   print(f"{self.name} is pursuing {self.courses}")
-
+  def enroll(self, course):
+    if course in self.courses:
+      print(f"{self.name} is already enrolled in {course.course_name}")
+      return
+    
+    self.courses.append(course)
+    print(f"{self.name} enrolled in {course.course_name}")
+    
+  def view_courses(self):
+    if not self.courses:
+      print("No courses enrolled yet.")
+      return 
+    
+    print(f"{self.name}'s Courses:")
+    for course in self.courses:
+      print(f"- {course.course_code}: {course.course_name}")
+    
 class Course:
   def __init__(self,course_code,course_name):
     self.course_code=course_code
     self.course_name=course_name
 
-course_1=Course(1100,"python")
-course_2=Course(1200,"c++")
-course_3=Course(1300,"javascript")
-course_4=Course(1400,"assembly")
